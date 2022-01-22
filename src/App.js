f827a7ce5be87d65de89a9bfb66bfa3dcb4b75bc
rh39807 <rh39807@gmail.com>
2022-01-22 13:24:17 -0600

    Sign up sheet and stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/home-page/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -17,9 +17,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=>console.log(this.state))
+        });
+      } else {
+        this.setState({ currentUser: userAuth }, ()=>console.log('no auth', this.state));
+      }
     })
   }
 
